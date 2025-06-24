76a58aec3db297a489aaeba81b3c513bcc86165d
Diego Figueroa <diego.figueroa@aurora.dev>
2025-06-24 08:15:34 -0400

    feat: add docblock to function
    Bump commit

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import { BYBIT_ADDRESSES } from './bybit-addresses';
 import { OLD_NEAR_ADDRESSES } from './old-near-addresses';
 
 /**
- * Helper function that checks presence of address in list of banned addresses
+ * Helper function that checks presence of address in list of banned addresses.
+ * See bin scripts for methodology.
  */
 export const isBannedNearAddress = (address: string): boolean =>
   OLD_NEAR_ADDRESSES.has(address) || BYBIT_ADDRESSES.has(address);
