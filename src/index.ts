b876d6e70dba37983355f268275052f6d3397343
Diego Figueroa <diego.figueroa@aurora.dev>
2025-06-24 07:49:21 -0400

    feat: document function
    Scaffold commit to trigger release.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { BYBIT_ADDRESSES } from './bybit-addresses';
 import { OLD_NEAR_ADDRESSES } from './old-near-addresses';
 
+/**
+ * Helper function that checks presence of address in list of banned addresses
+ */
 export const isBannedNearAddress = (address: string): boolean =>
   OLD_NEAR_ADDRESSES.has(address) || BYBIT_ADDRESSES.has(address);
